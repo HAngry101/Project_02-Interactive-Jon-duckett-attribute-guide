5e78dd49481c3d7bffce064ff3fc6ded98bea35b
HAngry101 <168013853+HAngry101@users.noreply.github.com>
2024-04-25 20:13:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-$("example_text").on("input propertychange", function(){
+$("#example_text").on("input propertychange",function(){
     var new_text = $("#example_text").val();
     $("#main_info").text(new_text);
-
-    if (new_text < 1) {
-        $("#main_info").text("ATTTRIBUTES TELL US MORE ABOUT ELEMENTS");
+    
+   /* var length = new_text.length;
+    $("#example_text").css("width", length + 150); */
+    
+    if(new_text < 1){
+       $("#main_info").text("ATTRIBUTES TELL US MORE ABOUT ELEMENTS");
     }
-});
-
-$("select").on("input propertychange", function(){
+  });
+  
+  $("select").on("input propertychange",function(){
     var color = $("select").val();
     $("#main_info").css("color", color);
-});
+  });
